5eb441ffb6619588d5cb590516ce6e6d093540a0
Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
2010-10-20 15:19:29 +0200

    Add GPLv3 notice in each source code file

--- src/streamops.c
+++ src/streamops.c
@@ -1,3 +1,20 @@
+/*
+	Copyright (C) 2010  EPFL (Ecole Polytechnique Fédérale de Lausanne)
+	Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
@@ -87,7 +104,7 @@ XDF_LOCAL int read_string_field(FILE* file, char* val, unsigned int nch)
 
 	// Remove trailing spaces
 	pos = strlen(val);
-	while (pos && (val[pos-1]==' '))
+	while (pos && (val[pos]==' '))
 		pos--;
 	val[pos] = '\0';
 
