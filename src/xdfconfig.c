13998b297e531f9e52660030c7b08de950a61733
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2013-02-10 14:30:18 +0100

    Update contact email

--- src/xdfconfig.c
+++ src/xdfconfig.c
@@ -1,7 +1,7 @@
 /*
     Copyright (C) 2010-2011  EPFL (Ecole Polytechnique Fédérale de Lausanne)
     Laboratory CNBI (Chair in Non-Invasive Brain-Machine Interface)
-    Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
+    Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
--- tests/errorcheck.c
+++ tests/errorcheck.c
@@ -1,7 +1,7 @@
 /*
     Copyright (C) 2010,2012  EPFL (Ecole Polytechnique Fédérale de Lausanne)
     Laboratory CNBI (Chair in Non-Invasive Brain-Machine Interface)
-    Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
+    Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
