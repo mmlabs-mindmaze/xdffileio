fb29c1eb0eec2c564970bf5224765235fee129ac
Marjorie Bournat <marjorie.bournat@mindmaze.com>
2019-09-24 15:25:13 +0200

    Make builddir configurable in test
    The tests can now be executed with autotools and soon with meson. For
    that, we have change the variables of environments in the Makefile and
    the script of tests.
    
    Change-Id: Id5658476c9fd4911514ff97ab6dd6dadab42d4ad

--- tests/recompose-gdf.sh
+++ tests/recompose-gdf.sh
@@ -3,13 +3,13 @@
 set -ex
 
 # gen broken.gdf, broken.gdf.code, broken.gdf.event
-$BUILDDIR/tests/gen-broken-gdf
+$EXECUTABLETEST/gen-broken-gdf
 
 # merge those 3 files into fixed.gdf
-$BUILDDIR/src/gdf-repair broken.gdf fixed.gdf
+$EXECUTABLESRC/gdf-repair broken.gdf fixed.gdf
 
 # test fixed.gdf contains all the expected data
-$BUILDDIR/tests/check-fixed-gdf
+$EXECUTABLETEST/check-fixed-gdf
 
 # remove generated files
 rm broken.gdf broken.gdf.code broken.gdf.event
